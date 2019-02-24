8eaaddb7bff684abb65b61221a3f0609b55f625d
Monica <mmpere14@asu.edu>
2019-02-23 18:59:09 -0700

    Initial version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Class Constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 *  Returns an integer that contain the current total.
+	 * @return The current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the parameter to the total variable
+	 * @param value An integer
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the parameter from the total variable
+	 * @param value an integer
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the total by the parameter
+	 * @param value an integer
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides the total by the parameter
+	 * @param value an integer
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns a String that displays a history of all previous actions
+	 * @return  A history of all actions as a String
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
